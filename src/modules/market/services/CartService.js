4440d6b21070db42511651072cdfbdaca63baf8b
martycabj1999 <martingira1999@gmail.com>
2020-08-21 21:33:18 -0300

    mercadopago integracion

--- src/modules/market/services/CartService.js
+++ src/modules/market/services/CartService.js
@@ -1,5 +1,6 @@
 import Cart from "../models/CartModel";
 import User from "../../security/models/UserModel";
+const mercadopago = require ('mercadopago');
 
 export async function readCarts () {
 
@@ -18,7 +19,7 @@ export async function addCart (products, total, total_discount = 0, user_id) {
         total_price: total,
         total_discount: total_discount,
         order_date: Date.now(),
-        state: 'available',
+        state: 'pending',
         user: user ? user : null,
     });
     
@@ -26,6 +27,51 @@ export async function addCart (products, total, total_discount = 0, user_id) {
     await cart.save()
 
     return cart;
+}
+
+export async function paymentMercadoPago (products, user_id, cart_id) {
+
+    // Agrega credenciales
+    mercadopago.configure({
+        access_token: 'TEST-8876112950783580-072719-b93d599dafda7b8e535584359c8548ef-143861807'
+    });
+
+    let items = []
+    let cartUpdate;
+
+    products.map( product => {
+        let item = {
+            id: product._id,
+            title: product.name,
+            currency_id: 'ARS',
+            unit_price: product.price,
+            quantity: product.quantity,
+        } 
+        items.push(item)
+    })
+
+    // Crea un objeto de preferencia
+    let preference = {
+        items: items
+    };
+    
+    await mercadopago.preferences.create(preference)
+    .then( async function(response){
+        // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+        global.id = response.body.user_id;
+        cartUpdate = await Cart.findByIdAndUpdate(cart_id, {
+            state: 'payed',
+        }) 
+    }).catch(async function(error){
+        console.error(error);
+        cartUpdate = await Cart.findByIdAndUpdate(cart_id, {
+            state: 'cancel',
+        }) 
+    });
+
+    cartUpdate = await Cart.findById(cart_id).populate('user')
+
+    return cartUpdate;
 }
 
 export async function getCart (id) {
